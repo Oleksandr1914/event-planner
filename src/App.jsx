941c79b74828fbae83e7e61968dde298c4c22bcb
Oleksandr_Mochakov <sasha.mochakov@ukr.net>
2023-08-01 09:38:16 +0300

    added color map,mixins and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Routes>
+        <Route index element={<div />} />
+        <Route path="/create-event" element={<div />} />
+        <Route path="/*" element={<Navigate to={'/'} />} />
+      </Routes>
     </>
   );
 }
---
+++ src/utils/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+  violet: 'rgba(123, 97, 255, 1)',
+  hight: 'rgba(255, 43, 119, 1)',
+  medium: 'rgba(226, 163, 0, 1)',
+  low: 'rgba(107, 212, 117, 1)',
+  text: 'rgba(63, 63, 63, 1)',
+  divider: 'rgba(172, 167, 195, 1)',
+};
